1552590a5cd640a3e09d9d3b24dc8d59afb7e9e8
machlord <mach.x@hotmail.com>
2021-07-07 22:16:02 -0300

    Removendo a classe watch da main
    Removendo a classe watch da main.

---
+++ Desafio/Watcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Desafio
+{
+    class Watcher : FileSystemWatcher
+    {
+        public Watcher()
+        {
+            this.Path = @"C:\Users\machx\Desktop\in";
+
+            this.NotifyFilter = NotifyFilters.Attributes
+                                   | NotifyFilters.CreationTime
+                                   | NotifyFilters.DirectoryName
+                                   | NotifyFilters.FileName
+                                   | NotifyFilters.LastAccess
+                                   | NotifyFilters.LastWrite
+                                   | NotifyFilters.Security
+                                   | NotifyFilters.Size;
+
+
+            this.Created += OnCreated;
+            this.Filter = "*.txt";
+            this.IncludeSubdirectories = true;
+            this.EnableRaisingEvents = true;
+        }
+
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string value = $"Created: {e.FullPath}";
+            Console.WriteLine(value);
+        }
+    }
+}
